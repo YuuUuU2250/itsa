2c4ac71166d4e99d6698987eea850a794897c03a
YuuUuU <118422132+YuuUuU2250@users.noreply.github.com>
2022-12-08 22:20:39 +0800

    Add files via upload

---
+++ Array II/C_AR95.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int ans[4], inp[4], A, B;
+    scanf("%1d%1d%1d%1d", &ans[0], &ans[1], &ans[2], &ans[3]);
+    do{
+        A = 0, B = 0;
+        scanf("%1d%1d%1d%1d", &inp[0], &inp[1], &inp[2], &inp[3]);
+        if(inp[0] || inp[1] || inp[2] || inp[3]){//輸入0000結束迴圈
+            for(int i=0; i<4; i++){
+                if(ans[i] == inp[i]){//位置正確
+                    A++;
+                }
+            }
+            if(A != 4){
+                for(int i=0; i<4; i++){
+                    for(int j=0; j<4; j++){
+                        if(i != j && ans[i] == inp[j]){//位置錯誤數字正確
+                            B++;
+                        }
+                    }
+                }
+            }
+            printf("%dA%dB\n", A, B);
+        }
+        else{
+            break;
+        }
+    }while(1);
+}
