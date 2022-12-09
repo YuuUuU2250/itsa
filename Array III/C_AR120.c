2a031b6c594b269b9cf6e20f2701499e278c86b5
YuuUuU <118422132+YuuUuU2250@users.noreply.github.com>
2022-12-09 16:58:34 +0800

    Add files via upload

---
+++ Array III/C_AR120.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+long long func(int);
+
+long long f[36] = {0};
+
+int main(){
+    for(int i=0; i<=35; i++){
+        f[i] = func(i);
+    }
+    int n;
+    scanf("%d", &n);
+    printf("f(%d)=%lld\n", n, f[n]);
+}
+
+long long func(int n){
+    if(n == 0){
+        return 1;
+    }
+    if(n == 1){
+        return 1;
+    }
+    long long ans = 0;
+    for(int i=0; i<n/2; i++){
+        ans += f[i] * f[n-1-i];
+    }
+    ans *= 2;
+    if(n%2){
+        ans += f[n/2] * f[n/2];
+    }
+    return ans;
+}
