8bf3aa8017209ef1b0c71177df710bd6d164652c
YuuUuU <118422132+YuuUuU2250@users.noreply.github.com>
2023-01-07 13:50:39 +0800

    Add files via upload

---
+++ Mathematics I/C_MM28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int inp;
+    scanf("%d", &inp);
+    inp /= 35;
+    for(int i=1; i<=inp; i++){
+        if(i - 1){
+            printf(" %d", i * 35);
+        }
+        else{
+            printf("%d", i*35);
+        }
+    }
+    printf("\n");
+}
