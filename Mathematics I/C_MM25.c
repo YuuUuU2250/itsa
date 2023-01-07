62aff754f3c0a21a3270e0453812e61c10af6962
YuuUuU <118422132+YuuUuU2250@users.noreply.github.com>
2023-01-07 22:15:51 +0800

    Update C_MM25.c

--- Mathematics I/C_MM25.c
+++ Mathematics I/C_MM25.c
@@ -4,7 +4,7 @@ int main(){
     int inp, outp;
     scanf("%d", &inp);
     inp /= 3;//最大可整除3的值
-    outp = inp * (inp+1) / 2;
+    outp = inp * (inp + 1) / 2;
     outp *= 3;
     printf("%d\n", outp);
 }
