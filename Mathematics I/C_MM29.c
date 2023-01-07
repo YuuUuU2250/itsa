b9b3e144e144204ac09067c65871f894f93900d2
YuuUuU <118422132+YuuUuU2250@users.noreply.github.com>
2023-01-07 14:17:58 +0800

    Add files via upload

---
+++ Mathematics I/C_MM29.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int inp;
+    scanf("%d", &inp);
+    int prime[inp], primes = 1;
+    prime[0] = 2;
+    for(int i=3; i<inp; i++){
+        for(int j=0; j<primes; j++){
+            if(!(i % prime[j])){
+                break;
+            }
+            if(j == primes - 1){
+                prime[primes++] = i;
+                break;
+            }
+        }
+    }
+    printf("%d\n", prime[--primes]);
+}
